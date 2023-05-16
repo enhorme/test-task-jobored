4b38925450574e21d87e8de18bda4db5952edb8b
Yauheni Pliuta <enhorme@gmail.com>
2023-05-16 19:47:30 +0200

    test preview image

--- src/components/MetaTags/index.js
+++ src/components/MetaTags/index.js
@@ -1,7 +1,6 @@
 import { useEffect, useState } from 'react'
 import { Helmet } from 'react-helmet-async'
 import { useLocation } from 'react-router-dom'
-import metaImg from '../../assets/images/meta.jpg'
 
 const MetaTags = () => {
   const [title, setTitle] = useState('home')
@@ -14,31 +13,8 @@ const MetaTags = () => {
   return (
     <Helmet>
       <title>{`Jobored | ${title}`}</title>
-      <meta name='description' content='Paralect Startup Summer 2023!' />
 
-      {/*Google / Search Engine Tags */}
-      <meta itemProp='name' content={`Jobored | ${title}`} />
-      <meta itemProp='description'
-            content='Paralect Startup Summer 2023!' />
-      <meta itemProp='image' content={metaImg} />
 
-      {/*Facebook Meta Tags*/}
-      <meta property='og:url'
-            content='https://jobored-paralect.netlify.app' />
-      <meta property='og:type' content='website' />
-      <meta property='og:title' content={`Jobored | ${title}`} />
-      <meta property='og:description'
-            content='Paralect Startup Summer 2023!' />
-      <meta property='og:image' content={metaImg} />
-
-      {/*Twitter Meta Tags*/}
-      <meta name='twitter:card'
-            content='summary_large_image' />
-      <meta name='twitter:title'
-            content={`Jobored | ${title}`} />
-      <meta name='twitter:description'
-            content='Paralect Startup Summer 2023!' />
-      <meta name='twitter:image' content={metaImg} />
     </Helmet>
   )
 }
