c9f2a31a78e20e05ea0a3346b5bc0e8a1d8fea15
Yauheni Pliuta <enhorme@gmail.com>
2023-05-16 20:43:14 +0200

    Change location title to uppercase

--- src/components/MetaTags/index.js
+++ src/components/MetaTags/index.js
@@ -3,18 +3,18 @@ import { Helmet } from 'react-helmet-async'
 import { useLocation } from 'react-router-dom'
 
 const MetaTags = () => {
-  const [title, setTitle] = useState('home')
+  const [title, setTitle] = useState('Home')
   const location = useLocation()
 
   useEffect(() => {
-    setTitle(location.pathname.split('/').at(1) || 'home')
+    const locName = location.pathname.split('/').at(1)
+    const titleFromLocation = locName.charAt(0).toUpperCase() + locName.slice(1)
+    setTitle(titleFromLocation || 'Home')
   }, [location])
 
   return (
     <Helmet>
       <title>{`Jobored | ${title}`}</title>
-
-
     </Helmet>
   )
 }
